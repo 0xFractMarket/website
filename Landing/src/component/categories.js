0993aa664c8768de63eddaaa49663690aa577947
Rolo 🐉 <55175623+RolandoDrRobot@users.noreply.github.com>
2025-03-30 23:38:26 -0600

    Set first layout for landing page (#1)

--- Landing/src/component/categories.js
+++ Landing/src/component/categories.js
@@ -4,7 +4,7 @@ import { categoriesData } from "./Properties/data";
 
 export default function Categories(){
     return(
-        <div className="grid lg:grid-cols-5 md:grid-cols-3 grid-cols-2 mt-8 md:gap-[30px] gap-3">
+        <div className="grid lg:grid-cols-4 md:grid-cols-4 grid-cols-2 mt-8 md:gap-[30px] gap-3">
             {categoriesData.map((item,index) =>{
                 return(
                     <div className="group rounded-xl bg-white dark:bg-slate-900 shadow hover:shadow-xl dark:hover:shadow-xl dark:shadow-gray-700 dark:hover:shadow-gray-700 overflow-hidden ease-in-out duration-500" key={index}>
