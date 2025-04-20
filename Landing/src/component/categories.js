1b3018ad4a012c8f6dc5f2da2f1887f82c635296
RolandoDrRobot <rolando.arguello.email@gmail.com>
2025-04-20 12:21:25 -0600

    Remove box shadow

--- Landing/src/component/categories.js
+++ Landing/src/component/categories.js
@@ -7,7 +7,7 @@ export default function Categories(){
         <div className="grid lg:grid-cols-4 md:grid-cols-4 grid-cols-2 mt-8 md:gap-[30px] gap-3">
             {categoriesData.map((item,index) =>{
                 return(
-                    <div className="group rounded-xl bg-white dark:bg-slate-900 shadow hover:shadow-xl dark:hover:shadow-xl dark:shadow-gray-700 dark:hover:shadow-gray-700 overflow-hidden ease-in-out duration-500" key={index}>
+                    <div className="group rounded-xl bg-white dark:bg-slate-900 shadow dark:shadow-gray-700 dark:hover:shadow-gray-700 overflow-hidden ease-in-out duration-500" key={index}>
                         <img src={item.image} alt="" />
                         <div className="p-4">
                             <Link to="#" className="text-xl font-medium hover:text-green-600">{item.name}</Link>
